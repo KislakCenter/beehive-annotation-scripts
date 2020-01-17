e9bc78641051f4013e8351f9b68385a3b70dc348
drnelson6 <drnelson6@gmail.com>
2020-01-17 10:10:43 -0500

    Added tracker for numerical section

--- problem-tracker.py
+++ problem-tracker.py
@@ -9,8 +9,9 @@ Created on Tue Oct 29 15:59:00 2019
 import csv
 import pandas as pd
 
+
 def alpha_annotation_maker(fl, pid, ref):
-    if fl.startswith(('A','B','C','D')):
+    if fl.startswith(('A', 'B', 'C', 'D')):
         return f"<a href='/digital-beehive/alpha1/{pid}/'>{ref}</a>"
     elif fl.startswith(('E', 'F', 'G', 'H')):
         return f"<a href='/digital-beehive/alpha2/{pid}/'>{ref}</a>"
@@ -21,8 +22,9 @@ def alpha_annotation_maker(fl, pid, ref):
     else:
         return f"<a href='/digital-beehive/alpha5/{pid}/'>{ref}</a>"
 
+
 def index_annotation_maker(fl, pid, ref):
-    if fl.startswith(('A','B','C','D')):
+    if fl.startswith(('A', 'B', 'C', 'D')):
         return f"<a href='/digital-beehive/index1/{pid}/'>{ref}</a>"
     elif fl.startswith(('E', 'F', 'G', 'H')):
         return f"<a href='/digital-beehive/index2/{pid}/'>{ref}</a>"
@@ -33,36 +35,59 @@ def index_annotation_maker(fl, pid, ref):
     else:
         return f"<a href='/digital-beehive/index5/{pid}/'>{ref}</a>"
 
-with open('beehive-data-sorted-for-wax.csv', 'r') as ip:
+
+def num_annotation_maker(entry, pid, ref):
+    if int(entry) <= 250:
+        return f"<a href='/digital-beehive/num1/{pid}/'>{ref}</a>"
+    elif int(entry) > 250:
+        return f"<a href='/digital-beehive/num2/{pid}/'>{ref}</a>"
+
+
+with open('beehive-data-for-wax.csv', 'r') as ip:
     bh_data = csv.DictReader(ip, delimiter=',')
     ids = {}
     for row in bh_data:
         ids.update({row['item']: row['pid']})
-        
+
 with open('alpha-issues.csv', 'r') as f:
     alpha_issues = pd.read_csv(f)
-    
+
 for row in alpha_issues.index:
-    item = alpha_issues.loc[row,'item']
-    entry = alpha_issues.loc[row,'entry']
-    first_letter = entry[0]
+    item = alpha_issues.loc[row, 'item']
+    entry = alpha_issues.loc[row, 'entry']
+    first_letter = alpha_issues.loc[row, 'first_letter']
     pid = ids[item]
     link = alpha_annotation_maker(first_letter, pid, entry)
-    alpha_issues.loc[row,'reference_link'] = link
-    
+    alpha_issues.loc[row, 'reference_link'] = link
+
 new_csv = alpha_issues.to_csv('alpha-issues.csv', index=False)
 print('Alphabetical section done.')
-      
+
 with open('index-issues.csv', 'r') as f:
     index_issues = pd.read_csv(f)
- 
+
 for row in index_issues.index:
     item = index_issues.loc[row, 'item']
     head = index_issues.loc[row, 'head']
     first_letter = index_issues.loc[row, 'first_letter']
     pid = ids[item]
     link = index_annotation_maker(first_letter, pid, head)
-    index_issues.loc[row, 'reference_link'] =link
-    
+    index_issues.loc[row, 'reference_link'] = link
+
 new_csv = index_issues.to_csv('index-issues.csv', index=False)
 print('Index done.')
+
+with open('num-issues.csv', 'r') as f:
+    num_issues = pd.read_csv(f)
+
+for row in num_issues.index:
+    item = num_issues.loc[row, 'item']
+    entry = str(num_issues.loc[row, 'entry'])
+    topic = num_issues.loc[row, 'topic']
+    pid = ids[item]
+    reference = f'{entry} [{topic}]'
+    link = num_annotation_maker(entry, pid, reference)
+    num_issues.loc[row, 'reference_link'] = link
+
+new_csv = num_issues.to_csv('num-issues.csv', index=False)
+print('Numerical section done.')
